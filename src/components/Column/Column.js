8ca8e2f375c482fa03accf3e60abe8390417448a
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-08-31 12:23:51 +0200

    Remove removeCard() and removeColumn()

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,20 @@
 import styles from './Column.module.scss'
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
   return (
     <article className={styles.column}>
       <div className={styles.title}>
         <h2><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
-        <button onClick={() => props.removeColumn(props.id)}>X</button>
+        <button>X</button>
       </div>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} id={card.id} card={card} columnId={props.id} cards={props.cards} title={card.title} removeCard={props.removeCard}/>)}
+        {cards.map(card => <Card key={card.id} id={card.id} card={card} columnId={props.id} cards={props.cards} title={card.title} removeCard={props.removeCard}/>)}
       </ul>
       <CardForm columnId={props.id} addCard={props.addCard} />
     </article>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,58 +1,15 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
-import { useState } from 'react';
-import shortid from 'shortid';
+import { useSelector } from 'react-redux';
 
 const List = () => {
 
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'Books',
-      icon: 'book',
-      cards: [
-        { id: 1, title: 'This is Going to Hurt' },
-        { id: 2, title: 'Interpreter of Maladies' }
-      ]
-    },
-    {
-      id: 2,
-      title: 'Movies',
-      icon: 'film',
-      cards: [
-        { id: 1, title: 'Harry Potter' },
-        { id: 2, title: 'Star Wars' }
-      ]
-    },
-    {
-      id: 3,
-      title: 'Games',
-      icon: 'gamepad',
-      cards: [
-        { id: 1, title: 'The Witcher' },
-        { id: 2, title: 'Skyrim' }
-      ]
-    }
-  ]);
+  const columns = useSelector(state => state.columns);
 
-  const addColumn = newColumn => {
-    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
-  };
-
-  const removeColumn = columnId => {
+/*  const removeColumn = columnId => {
     setColumns(columns.filter(column => column.id !== columnId));
   };
-
-  const addCard = (newCard, columnId) => {
-    const columnsUpdated = columns.map(column => {
-      if(column.id === columnId)
-        return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
-      else
-        return column
-        })
-        setColumns(columnsUpdated);
-      };
 
   const removeCard = (columnId, cardId) => {
     const columnsUpdated = columns.map(column => {
@@ -63,7 +20,7 @@ const List = () => {
         })
     setColumns(columnsUpdated);
   };
-
+*/
     return (
       <div className={styles.list}>
         <header className={styles.header}>
@@ -72,19 +29,12 @@ const List = () => {
         <p className={styles.description}>Interesting things I want to check out.</p>
         <section className={styles.columns}>
           {columns.map(column =>
-                <Column addCard={addCard} 
-                setColumns={setColumns} 
-                removeCard={removeCard} 
-                removeColumn={removeColumn}
-                column={column}
+                <Column 
                 key={column.id}
-                id={column.id}
-                title={column.title}
-                icon={column.icon}
-                cards={column.cards}
-          />)}
+                {...column} />
+                )}
         </section>
-        <ColumnForm action={addColumn} />
+        <ColumnForm />
       </div>
     );
   };
